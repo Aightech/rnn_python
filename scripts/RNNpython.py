36b2694c7fe7fad6a5952423a762b48651776aa3
Elias Hanna <h.elias@hotmail.fr>
2018-12-19 18:26:04 +0100

    Added offline and online routine

--- scripts/RNNpython.py
+++ scripts/RNNpython.py
@@ -11,8 +11,8 @@ import numpy as np
 
 # Read from topics values
 lasers = list()
-speed_left = 0
-speed_right = 0
+speed_left = Float32()
+speed_right = Float32()
 
 # Rate for ROS in Hz
 rate = 1000
@@ -20,7 +20,7 @@ l_range = 100
 
 class RNN_module:
 
-    def __init__(self, num, nb_sensor, nb_motor, scaling, learning_rate=0.9, hidden_dim=4, bptt_truncate=4):
+    def __init__(self, num, nb_sensor, nb_motor, scaling, learning_rate=0.9, hidden_dim=4, bptt_truncate=20):
         # Assign instance variables
         self.num = num
         self.gate_state = 0
@@ -49,7 +49,7 @@ class RNN_module:
 
     #-------------------------------------------
     # Updates attributes of RNN class
-    def update_attributes(self, gate_opening, gate_state):
+    def update_gate(self, gate_opening, gate_state):
         self.set_gate_opening(gate_opening)
         self.set_gate_state(gate_state)
 
@@ -97,6 +97,12 @@ class RNN_module:
         for t in range(0, T):
             self.S[:,t] = np.tanh(self.U.dot(self.window[t][:]) + self.W.dot(self.S[:,t-1]))
             output[:,t] = self.V.dot(self.S[:,t])
+        # Normalization of the output
+        for i in range(0, len(output)):
+            sum = 0
+            for j in output[i]:
+                sum += j**2
+            output[i] = output[i]/math.sqrt(sum)
         return output   # Return output on the current time window
 
     #-------------------------------------------
@@ -191,12 +197,11 @@ class RNN_module:
 
 #-------------------------------------------
 class ExpertMixture:
-    def __init__(self, data, epsilon_g, nu_g, scaling, RNN_number=5):
+    def __init__(self, epsilon_g, nu_g, scaling, RNN_number=5):
         self.epsilon_g = epsilon_g
         self.nu_g = nu_g
         self.scaling = scaling
-        self.data = data
-        self.error = np.zeros((RNN_number, len(data[0])))
+        
         self.s_t = collections.deque(maxlen=2)
         self.g_t = np.zeros(RNN_number)
         self.RNN_number = RNN_number
@@ -204,6 +209,13 @@ class ExpertMixture:
         self.RNNs = list()
         for i in range(0, RNN_number):
             self.RNNs.append(RNN_module(num=i, nb_sensor=6, nb_motor=2, scaling=scaling))
+
+        self.pub_error = list()
+        self.pub_gate = list()
+        for number in range (1, self.RNN_number + 1):
+            self.pub_error.append(rospy.Publisher('/MRE/error_'+str(number), Float32 , queue_size=10))
+        for number in range (1, self.RNN_number + 1):
+            self.pub_gate.append(rospy.Publisher('/MRE/gate_'+str(number), Float32 , queue_size=10))
 
     #-------------------------------------------
     # Target et output contiennent les valeurs obtenues du robot/simulation et celles de sortie du RNN respectivement
@@ -240,13 +252,18 @@ class ExpertMixture:
         return (self.epsilon_g * self.compute_partial_L_s_k(index) - self.nu_g * (self.s_t[1][index] - self.s_t[0][index]))
 
     #-------------------------------------------
-    def routine(self):
-        pub_error = list()
-        pub_gate = list()
-        for number in range (1, self.RNN_number + 1):
-            pub_error.append(rospy.Publisher('/MRE/error_'+str(number), Float32 , queue_size=10))
-        for number in range (1, self.RNN_number + 1):
-            pub_gate.append(rospy.Publisher('/MRE/gate_'+str(number), Float32 , queue_size=10))
+    def list_sum(self, a, b):
+        if (len(a) != len(b)):
+            print "Error in list_sum: lists are not the same size"
+            return []
+        ret = []
+        for i in range(0, len(a)):
+            ret.append(a[i] + b[i])
+        return ret
+    #-------------------------------------------
+    def routine_offline(self, data):
+        self.data = data
+        self.error = np.zeros((self.RNN_number, len(data[0])))
 
         tmp = [0 for i in range(0,self.RNN_number)]
         self.s_t.append(tmp)
@@ -255,7 +272,7 @@ class ExpertMixture:
                 o = self.RNNs[i].predict(self.data[t])
                 self.error[i] = np.subtract(np.array(self.data[t]),o)
                 # Publish the error values
-                pub_error[i].publish(self.norm_2_i(i))
+                self.pub_error[i].publish(self.norm_2_i(i))
                 tmp[i] = self.RNNs[i].get_gate_state()
 
             self.s_t.append(tmp)
@@ -263,14 +280,36 @@ class ExpertMixture:
             for i in range(0, self.RNN_number):
                 self.g_t[i] = self.compute_gt_i(i)
                 # Publish the gating values
-                pub_gate[i].publish(self.g_t[i])
+                self.pub_gate[i].publish(self.g_t[i])
 
             for i in range(0, self.RNN_number):
-                tmp[i] += self.compute_delta_s_k_i(i)
-                self.RNNs[i].set_gate_state(self.s_t[1][i])
-                self.RNNs[i].update_attributes(self.g_t[i], self.s_t[1][i])
-                self.s_t[0][i] = self.s_t[1][i]
-            self.s_t.append(tmp)
+                self.s_t[1][i] = self.s_t[1][i] + self.compute_delta_s_k_i(i)
+                self.RNNs[i].update_gate(self.g_t[i], self.s_t[1][i])
+
+    #-------------------------------------------
+    def routine_online(self, data):
+        self.data = data
+        self.error = np.zeros((self.RNN_number, len(data)))
+
+        tmp = [0 for i in range(0,self.RNN_number)]
+        self.s_t.append(tmp)
+        for i in range(0, self.RNN_number):
+            o = self.RNNs[i].predict(self.data)
+            self.error[i] = np.subtract(np.array(self.data),o)
+            # Publish the error values
+            self.pub_error[i].publish(self.norm_2_i(i))
+            tmp[i] = self.RNNs[i].get_gate_state()
+
+        self.s_t.append(tmp)
+
+        for i in range(0, self.RNN_number):
+            self.g_t[i] = self.compute_gt_i(i)
+            # Publish the gating values
+            self.pub_gate[i].publish(self.g_t[i])
+
+        for i in range(0, self.RNN_number):
+            self.s_t[1][i] = self.s_t[1][i] + self.compute_delta_s_k_i(i)
+            self.RNNs[i].update_gate(self.g_t[i], self.s_t[1][i])
 
 #-------------------------------------------
 def callback_lasers(data):
@@ -329,19 +368,16 @@ def data_to_window(data, time, window_size):
     elif(time > 0):
         return data[(time - window_size +2):time + 2]   # Return a window vector of [t-2, t-1, t, t+1] according to current time
     print "Error in data_to_window: check time input"
-    
+
 #-------------------------------------------
-def extend_data(data):
-    return data + data + data
+def extend_data(data, n):
+    if(n == 0):
+        return data
+    return data + extend_data(data, n - 1)
 
 #-------------------------------------------
 def online_learning():
     rospy.init_node('online_learning', anonymous=True)
-
-    # The node publishes the gating values so that rqt_plot can plot those :
-    d={}
-    # for number in range (1, 6):
-    #     d["pub_gate_{0}".format(number)] = rospy.Publisher('/MRE/gate_'+str(number), Float32 , queue_size=10)
 
     # The node receives sensory and motor information from simu_fastsim:
     rospy.Subscriber("/simu_fastsim/lasers", Float32MultiArray, callback_lasers)
@@ -349,40 +385,52 @@ def online_learning():
     rospy.Subscriber('/simu_fastsim/speed_right', Float32, callback_speed_right)
 
     # Targetted operating frequency of the node:
-    r = rospy.Rate(rate) # 10hz
+    r = rospy.Rate(rate) # rate in Hz
 
-    # Import the data
+    ##########      Import the data
+
     rospack = rospkg.RosPack()
 
     # Get the file path for rospy_tutorials
     path = rospack.get_path('RNN_python')
 
     # Read left motor data from csv file
-    m_left = {}
-    read_csv(path + '/data/_slash_simu_fastsim_slash_speed_left.csv', m_left, list_flag=False)
+    m_left_data = {}
+    read_csv(path + '/data/_slash_simu_fastsim_slash_speed_left.csv', m_left_data, list_flag=False)
 
     # Read right motor data from csv file
-    m_right = {}
-    read_csv(path + '/data/_slash_simu_fastsim_slash_speed_right.csv', m_right, list_flag=False)
+    m_right_data = {}
+    read_csv(path + '/data/_slash_simu_fastsim_slash_speed_right.csv', m_right_data, list_flag=False)
 
     # Read lasers data from csv file
-    lasers = {}
-    read_csv(path + '/data/_slash_simu_fastsim_slash_lasers.csv', lasers, list_flag=True)
+    lasers_data = {}
+    read_csv(path + '/data/_slash_simu_fastsim_slash_lasers.csv', lasers_data, list_flag=True)
 
     # Align the data
-    training_data = align_data(m_left, m_right, lasers)
+    training_data = align_data(m_left_data, m_right_data, lasers_data)
+
+    # Extend the data
+    # training_data = extend_data(training_data, 5)
 
     # Start time and timing related things
     startT = rospy.get_time()
     rospy.loginfo("Start time: " + str(startT))
 
-    mre = ExpertMixture(training_data, 0.007, 0.02, 10)
-    mre.routine()
+    ##########      Learn
+
+    mre = ExpertMixture(0.007, 0.02, 0.1)
+    # mre.routine_offline(training_data)
         
 
     while (not rospy.is_shutdown()):
-        # d["pub_gate_1"].publish(1)
-        pass
+        data = []
+        data += [speed_left.data]
+        data += [speed_right.data]
+        data += lasers[1:len(lasers)-1]
+        print data
+        if(len(data) == 8):
+            mre.routine_online(data)
+            pass
 
 #-------------------------------------------
 if __name__ == '__main__':
